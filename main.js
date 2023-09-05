638280ed69ccd595d904975a4bc678a697dec017
lucasuthh <lucasuthh@gmail.com>
2023-09-05 11:42:16 -0600

    Cambios Responsive landingPageTwo

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+// loading
 window.addEventListener("load", function(){
     this.document.getElementById("loader").classList.toggle("loader2")
 });
+
+
+// Obtener el checkbox y el menú principal
+const checkbox = document.getElementById("check");
+const mainmenu = document.getElementById("mainmenu");
+
+// Agrega un evento de cambio al checkbox para mostrar/ocultar el menú
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    mainmenu.classList.add("active");
+  } else {
+    mainmenu.classList.remove("active");
+  }
+});
+
+
+
 
